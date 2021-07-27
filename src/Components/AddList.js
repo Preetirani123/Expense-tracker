02e2050667a81b42f79a7f1c306bee4c26ca6422
Preetirani123 <rani.preeti3333@gmail.com>
2021-07-27 05:18:56 +0000

    add the add expense from fom functionality

--- src/Components/AddList.js
+++ src/Components/AddList.js
@@ -1,14 +1,29 @@
-import React, {useState} from 'react'
+import React, {useState, useContext} from 'react';
+import { GlobalContent } from '../Helper/Global';
 
 export const AddList = () => {
   const [text, setText] = useState('');
   const [category, setCategory] = useState('');
   const [amount, setAmount] = useState(0);
 
+  const {addList} = useContext(GlobalContent)
+
+  const onSubmit = e => {
+    e.preventDefault();
+
+    const newList = {
+      id : Math.floor(Math.random() * 100000000),
+      text,
+      category,
+      amount: +amount
+    }
+    addList(newList);
+  }
+
   return (
     <>
     <h3>Expense List Form</h3>
-    <form >
+    <form onSubmit={onSubmit}>
       <div className="form-control"> 
         <label htmlFor="text">Name </label>
         <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter name of the expense"/>
@@ -21,6 +36,7 @@ export const AddList = () => {
         <label htmlFor="amount">Amount</label>
         <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)}  placeholder="Enter Amount"/>
       </div>
+      <button className="btn">Add</button>
     </form>  
     </>
   )
