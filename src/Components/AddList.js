409a9e43ae323c699a886f6971434132201d76df
Preetirani123 <rani.preeti3333@gmail.com>
2021-07-27 03:08:02 +0000

    push updated code

--- src/Components/AddList.js
+++ src/Components/AddList.js
@@ -1,21 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export const AddList = () => {
+  const [text, setText] = useState('');
+  const [category, setCategory] = useState('');
+  const [amount, setAmount] = useState(0);
+
   return (
     <>
     <h3>Expense List Form</h3>
     <form >
       <div className="form-control"> 
         <label for="text">Name </label>
-        <input type="text"  placeholder="Enter name of the expense"/>
+        <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter name of the expense"/>
       </div>
       <div className="form-control"> 
         <label for="text">Category </label>
-        <input type="text"  placeholder="Enter Category of the purchase"/>
+        <input type="text"  value={category} onChange={(e) => setCategory(e.target.value)} placeholder="Enter Category of the purchase"/>
       </div>
       <div className="form-control"> 
         <label for="amount">Amount</label>
-        <input type="number"  placeholder="Enter Amount"/>
+        <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)}  placeholder="Enter Amount"/>
       </div>
     </form>  
     </>
